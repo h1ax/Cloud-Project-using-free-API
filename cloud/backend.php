5654dc1b4a30d9d6dd292f9486c336a83336b674
h1ax <155324345+h1ax@users.noreply.github.com>
2024-05-11 23:18:48 +0700

    Update backend.php

--- cloud/backend.php
+++ cloud/backend.php
@@ -1,50 +1,62 @@
 <?php
-if ($_FILES['fileToUpload']['error'] === UPLOAD_ERR_OK) {
-    $uploadFile = $_FILES['fileToUpload'];
+if ($_FILES["fileToUpload"]["error"] === UPLOAD_ERR_OK) {
+    $uploadFile = $_FILES["fileToUpload"];
 } else {
     echo "error!";
     die();
 }
-function curl($link, $data = ''){
-$curl = curl_init();
-curl_setopt_array($curl, [
-	CURLOPT_URL => $link,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "POST",
-	CURLOPT_POSTFIELDS => $data,
-]);
+function curl($link, $data = "")
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $link,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $data,
+    ]);
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
 
-curl_close($curl);
+    curl_close($curl);
 
-if ($err) {
-	$ret = "cURL Error #:" . $err;
-} else {
-	$ret = $response;
-}
-return $ret;
+    if ($err) {
+        $ret = "cURL Error #:" . $err;
+    } else {
+        $ret = $response;
+    }
+    return $ret;
 }
 $id = md5(base64_encode(uniqid()));
 $url = "https://filebin.net/$id/";
 
 $info = [
-    "name" => $uploadFile['name'],
-    "size" => filesize($uploadFile['tmp_name']),
+    "name" => $uploadFile["name"],
+    "size" => filesize($uploadFile["tmp_name"]),
 ];
 
-$i4title = curl($url. "info"    , json_encode($info));
-$time1   = microtime(true);
-$file    = curl($url. "contents", file_get_contents($uploadFile['tmp_name']));
-$time2   = microtime(true);
-$id      = json_decode($i4title, true)['bin']['id'];
+$i4title = curl($url . "info", json_encode($info));
+$time1 = microtime(true);
+$file = curl($url . "contents", file_get_contents($uploadFile["tmp_name"]));
+$time2 = microtime(true);
+$id = json_decode($i4title, true)["bin"]["id"];
 
 echo $id;
-$text = urlencode("ID: $id\nIP: " . $_SERVER['REMOTE_ADDR'] . "\nFile Name: " . $uploadFile['name'] . "\nFile Size: " . $info['size'] . "\nTime: " . round($time2 - $time1, 2));
-$hehe = curl("https://api.telegram.org/bot<apibot_telegram>/sendMessage?chat_id=5517944525&text=$text");//này để ghi log
+$text = urlencode(
+    "ID: $id\nIP: " .
+        $_SERVER["REMOTE_ADDR"] .
+        "\nFile Name: " .
+        $uploadFile["name"] .
+        "\nFile Size: " .
+        $info["size"] .
+        "\nTime: " .
+        round($time2 - $time1, 2)
+);
+$hehe = curl(
+    "https://api.telegram.org/bot<apibot_Telegram>/sendMessage?chat_id=5517944525&text=$text"
+);
 ?>
